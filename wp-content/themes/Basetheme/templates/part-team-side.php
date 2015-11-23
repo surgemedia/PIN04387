4d618d260c4c94f87631aad569d14240c0f1c1c5
Hector Navarro <hector@surgemedia.com.au>
2015-11-23 10:51:32 +1000

    include for team side and quick links

---
+++ wp-content/themes/Basetheme/templates/part-team-side.php
@@ -0,0 +1,37 @@
+    <?php
+    $team_side = get_field($team_side_column);
+    
+    // WP_Query arguments
+    $args = array(
+        'post_type' => array('team'),
+        'post__in' =>  $team_side,
+    );
+    
+    // The Query
+    $query = new WP_Query($args);
+    
+    // The Loop
+    if ($query->have_posts()) {
+        while ($query->have_posts()) {
+            $query->the_post(); ?>
+            <?php $image = getFeaturedUrl(get_the_id()); 
+            $image_url = aq_resize($image,100,100,true,true,true); ?>
+            <?php debug($image_url); ?>
+            <?php debug(get_the_title()); ?>
+            <?php debug(get_field('job_title')); ?>
+            <?php debug(get_field('phone')); ?>
+            <?php debug(get_field('mobile')); ?>
+            <?php debug(get_field('email')); ?>
+         <?php
+        }
+    } 
+    else {
+        
+        // no posts found
+        
+    }
+    
+    // Restore original Post Data
+    unset($team_side);
+    wp_reset_postdata();
+?>
