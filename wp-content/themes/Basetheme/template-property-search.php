d6323689cb7e8022739ed9a994d4f68be1fd13cd
Hector Navarro <hector@surgemedia.com.au>
2015-12-11 09:05:59 +1000

    Styling and template defiant,

--- wp-content/themes/Basetheme/template-property-search.php
+++ wp-content/themes/Basetheme/template-property-search.php
@@ -4,21 +4,7 @@
  * Template Name: Property Search Template
  */
 ?>
-<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.templates/beta1/jquery.tmpl.js"></script>
-<script id="propertyTemplate" type="text/x-jQuery-tmpl">
-<article class="property-card col-lg-6" style="background:url('http://localhost/PinnacleProperties/wp-content/uploads/2015/11/sale-960x730.png');">
-    
-    <div class="content">
-        <div class="suburb">${title}</div>
-        <div class="street">${address}</div>
-        <ul class="amenity">
-            <li><i class="glyphicon glyphicon-envelope"><div>2</div></i></li>
-            <li><i class="glyphicon glyphicon-envelope"><div>2</div></i></li>
-            <li><i class="glyphicon glyphicon-envelope"><div>2</div></i></li>
-        </ul>
-    </div>
-</article>
-    </script>
+
 <?php 
 // no default values. using these as examples
 $taxonomies = array( 
@@ -51,55 +37,141 @@ $args = array(
     $terms = get_terms($taxonomies, $args);
    // debug($terms);
  ?>
-<section id="search">
-    <label for="suburb">REFINE SEARCH TERMS</label>
-    <input type="text" name="suburb">
-    <button onclick="load()">SEARCH</button>
-    <input type="checkbox" name="surrounding" value="include"> INCLUDE SURROUNDING PROPERTIES
-    
-    <label for="">PROPERTY TYPE</label>
-    <select name="" id="type">
-     <option value="">- Any -</option>
-    <?php 
-    for ($i=0; $i <= sizeof($terms); $i++) { 
-        echo '<option value='.$terms[$i]->slug.'>'.$terms[$i]->name.'</option>';
-    }
-     ?>
-    </select>
-    
-    <label for="">BEDROOMS</label>
-    <select name="" id="bed">
-     <option value="">- Any -</option>
-    <?php 
-    for ($i=0; $i <= sizeof($terms); $i++) { 
-        echo '<option value='.$terms[$i]->slug.'>'.$terms[$i]->name.'</option>';
-    }
-     ?>
-    </select>
-    
-    <label for="">BATHROOMS</label>
-    <select name="" id="bath">
-     <option value="">- Any -</option>
-    <?php 
-    for ($i=0; $i <= sizeof($terms); $i++) { 
-        echo '<option value='.$terms[$i]->slug.'>'.$terms[$i]->name.'</option>';
-    }
-     ?>
-    </select>
-    
-    <label for="">CAR SPACES</label>
-    <select name="" id="car">
-     <option value="">- Any -</option>
-    <?php 
-    for ($i=0; $i <= sizeof($terms); $i++) { 
-        echo '<option value='.$terms[$i]->slug.'>'.$terms[$i]->name.'</option>';
-    }
-     ?>
-    </select>
 
+<div class="search-header row">
+    <section id="search" class="col-lg-6">
+        <div class="filter">
+            <label for="suburb">REFINE SEARCH TERMS</label>
+            <input type="text" name="suburb" id="suburb">
+            <button onclick="search();">SEARCH</button>
+        </div>
+        
+        <div class="surrounding">
+            <input type="checkbox" name="surrounding" value="include"> INCLUDE SURROUNDING PROPERTIES
+        </div>
+        
+        <div class="field">
+            <label for="">PROPERTY TYPE</label>
+            <select name="" id="type">
+             <option value="">- Any -</option>
+              
+                
+            <?php 
+            for ($i=0; $i <= sizeof($terms); $i++) { 
+                echo '<option value='.$terms[$i]->slug.'>'.$terms[$i]->name.'</option>';
+            }
+             ?>
+            </select>
+        </div>
+        
+        <div class="field">
+            <label for="">BEDROOMS</label>
+            <select name="" id="bed">
+             <option value="">- Any -</option>
+             <option value="1">- 1 + -</option>
+             <option value="2">- 2 + -</option>  
+             <option value="3">- 3 + -</option>  
+            <?php 
+            /*for ($i=0; $i <= sizeof($terms); $i++) { 
+                echo '<option value='.$terms[$i]->slug.'>'.$terms[$i]->name.'</option>';
+            }*/
+             ?>
+            </select>
+        </div>
+        
+        <div class="field">
+            <label for="">BATHROOMS</label>
+            <select name="" id="bath">
+             <option value="">- Any -</option>
+             <option value="1">- 1 + -</option>
+             <option value="2">- 2 + -</option>  
+             <option value="3">- 3 + -</option>  
+            <?php 
+            /*for ($i=0; $i <= sizeof($terms); $i++) { 
+                echo '<option value='.$terms[$i]->slug.'>'.$terms[$i]->name.'</option>';
+            }*/
+             ?>
+            </select>
+        </div>
+        
+        <div class="field">
+            <label for="">CAR SPACES</label>
+            <select name="" id="car">
+             <option value="">- Any -</option>
+             <option value="1">- 1 + -</option>
+             <option value="2">- 2 + -</option>  
+             <option value="3">- 3 + -</option> 
+            <?php 
+                   /* for ($i=0; $i <= sizeof($terms); $i++) { 
+                echo '<option value='.$terms[$i]->slug.'>'.$terms[$i]->name.'</option>';
+            }*/
+             ?>
+            </select>
+        </div>
+    </section>
+    <section class="search-feature col-lg-6 row">
+        <?php  
+                    // WP_Query arguments
+                $args = array (
+                    'post_type' => array( 'property' ),
+                    'p' => get_field('featured_property')[0],
+                    ); 
+                $query = new WP_Query( $args );
+                
+                if ( $query->have_posts() ) {
+                    while ( $query->have_posts() ) {
+                        $query->the_post();
+                        // debug(get_post_meta(get_the_id() ));?>
+                         <?php 
+                    // $the_property = get_post(get_the_id() );
+                    $the_property = get_post();
+                    $the_property_meta = get_post_meta(get_the_id() );
+                     ?>
+                    <div class="col-sm-6">
+                        <?php 
+                             $image = getFeaturedUrl(get_the_id()); 
+                             $image_url = aq_resize($image,960,621,true,true,true);
+                        ?>
+                        <img src="<?php echo $image_url; ?>" alt="<?php the_title(); ?>">
+                        <h1 class="title"><strong>Propery</strong> of the week</h1>
+                    </div>
+                    <div class="col-sm-6 ">   
+                            <div class="property-obj">
+                                <div class="suburb"><?php echo $the_property_meta['property_address_suburb'][0]; ?></div>
+                                <div class="street">
+                                    <?php echo $the_property_meta['property_address_street_number'][0]; ?> &nbsp;
+                                    <?php echo $the_property_meta['property_address_street'][0]; ?>
+                                </div>
+                                <ul>
+                                    <li><i class="icon-BED"></i>
+                                        <span><?php echo $the_property_meta['property_bedrooms'][0]; ?></span>
+                                    </li>
+                                    <li><i class="icon-BATH"></i>
+                                        <span><?php echo $the_property_meta['property_bathrooms'][0]; ?></span>
+                                    </li>
+                                    <li><i class="icon-CAR"></i>
+                                        <span><?php echo $the_property_meta['property_garage'][0]; ?></span>
+                                    </li>
+                                </ul>
+                                
+                                <a href="<?php echo $the_property->guid; ?>" class="details">Details</a>
+
+                            </div>
+                        </div>   
+                
+
+
+                <?php    }
+                } else {
+                    // no posts found
+                }
+    
+                wp_reset_postdata(); ?>
+        
+    <section>
+</div>
+    
 </section>
-
-
 <div id="blogPostContainer"> </div>
 
 <!-- Defiant template -->
@@ -107,70 +179,42 @@ $args = array(
 <xsl:template name="property">
     
     <xsl:for-each select="//property">
-        <article class="property-card col-lg-6" style="background:url('<?php echo wp_get_attachment_image_src("{featured_image}")[0] ?> ');">
+        <article class="property-card col-lg-6" style="background:url('{property_image}');">
         
-        <div class="content">
+        <a href="{link}" class="content">
             <div class="suburb"><xsl:value-of select="property_meta/property_address_suburb"/></div>
             <div class="street">
-                <xsl:value-of select="property_meta/property_address_street_number"/>
-                <xsl:value-of select="property_meta/property_address_street"/>
+                <span>
+                    <xsl:value-of select="property_meta/property_address_street_number"/>
+                </span>
+                <span>
+                    <xsl:value-of select="property_meta/property_address_street"/>
+                </span>
             </div>
-            <ul class="amenity">
-                <li class="col-lg-4"><i class="glyphicon glyphicon-envelope">
-                        <div><xsl:value-of select="property_meta/property_rooms"/>
-                        </div>
-                    </i>
-                </li>
-                <li class="col-lg-4"><i class="glyphicon glyphicon-envelope">
-                        <div><xsl:value-of select="property_meta/property_garage"/>
-                        </div>
-                    </i>
-                </li>
-                <li class="col-lg-4"><i class="glyphicon glyphicon-envelope">
-                        <div><xsl:value-of select="property_meta/property_toilet"/>
-                        </div>
-                    </i>
-                </li>
-            </ul>
-        </div>
+            <i class="icon-BED">
+                    <xsl:value-of select="property_meta/property_bedrooms"/>
+                </i>
+            <i class="icon-BATH">
+                    <xsl:value-of select="property_meta/property_bathrooms"/>
+                    
+                </i>
+            <i class="icon-CAR">
+                    <xsl:value-of select="property_meta/property_garage"/>
+                    
+                </i>
+        </a>
     </article>
 
     </xsl:for-each>
     
 </xsl:template>
 
-<xsl:template name="tree">
-    <h1>Tree</h1>
-    <xsl:call-template name="tree-walker"/>
-</xsl:template>
-
-<xsl:template name="tree-walker">
-    <xsl:param name="indent" />
-    <xsl:for-each select="./*">
-        <xsl:sort order="descending" select="count(./*)"/>
-        <div class="tree-item">
-            <xsl:value-of select="$indent"/> <xsl:value-of select="@name"/>
-            <xsl:if test="count(./*) > 0">
-                <div class="item-children">
-                    <xsl:call-template name="tree-walker">
-                        <xsl:with-param name="indent">
-                            <xsl:value-of select="$indent"/>&#160;&#160;
-                        </xsl:with-param>
-                    </xsl:call-template>
-                </div>
-            </xsl:if>
-        </div>
-    </xsl:for-each>
-</xsl:template>
-
 </script>
 
 <!-- Output element -->
-<pre id="output"></pre>
+<div id="output" class="row"></div>
 
 
-
-<pre id="jsonConsole" ></pre>
     <?php
 //     global $wp_post_types;
 // // debug( $wp_post_types[ 'property' ] );
