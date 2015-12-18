89a0e94f22bdea91c70252fba906222ea573d608
Hector Navarro <hector@surgemedia.com.au>
2015-12-18 10:28:22 +1000

    tips without image and odd even color

--- wp-content/themes/Basetheme/template-tips.php
+++ wp-content/themes/Basetheme/template-tips.php
@@ -13,16 +13,15 @@
 	$postTitle="";
 	include(locate_template('templates/part-jumbotron.php')); ?>
 	
-	
+	<?php while (have_posts()) : the_post(); ?>
 <div class="tips-content">
 		<div class="content">
-			“I just want to say a belated thanks to the team at Pinnacle Properties and to recommend their service to anyone looking to sell their property! It was an absolute pleasure selling my house  with Von. She is always very prompt, positive and helpful.”
+			<?php the_content(); ?>
 
 		</div>
-			<div class="author">
-				Martin Shane
-			</div>
+			
 	</div>
+	<?php endwhile; ?>
 <?php 
 // WP_Query arguments
 $args = array (
@@ -33,20 +32,21 @@ $args = array (
 // The Query
 $the_query = new WP_Query( $args );?>
 <?php if ( $the_query->have_posts() ) {
-      
+    
+  $color="";    
       while ( $the_query->have_posts() ) {
         $the_query->the_post();?>
 
 <?php 
-	// echo "string 1";
-	debug(get_post());
-	$color="grey-dark";
-	$image=getFeaturedUrl(get_the_id());
 
-	include(locate_template('templates/part-tips-card.php')); ?>
+	
+	$image=wp_get_attachment_url( get_post_thumbnail_id($post->ID));
+	// debug($image);
+	include(locate_template('templates/part-tips-card.php')); 
 
-
-
+	if (""==$color) $color = 'grey-dark';
+      else $color = "";
+	?>
 <?php 
   }
   
--- wp-content/themes/Basetheme/templates/part-tips-card.php
+++ wp-content/themes/Basetheme/templates/part-tips-card.php
@@ -1,14 +1,18 @@
 
-<?php if (""==$image){
+<?php if (""!=$image){
     $color="";
-} 
+    $content_size="";
+}else{
+    $content_size="big";
+}
 ?>
 <div class="card col-lg-4 <?php echo $color; ?>">
-    
-    <div class="head-card">
-        <img src="<?php echo $image; ?>" alt="">
-    </div>
-    <div class="content-card">
+    <?php if (""!=$image):?>
+        <div class="head-card">
+            <img src="<?php echo $image; ?>" alt="">
+        </div>
+    <?php endif;?>
+    <div class="content-card <?php echo $content_size ?>">
         <div class="title"><?php the_title(); ?></div>
         <p><?php the_content(); ?></p>
     </div>
