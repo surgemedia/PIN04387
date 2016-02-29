fa10a5113a47761d6f98b2c0db30d7ab358d02c7
Hector Navarro <hectornucv@gmail.com>
2016-02-29 12:29:48 +1000

    Quick links with External links and File Links

--- wp-content/themes/Basetheme/templates/part-quick-links.php
+++ wp-content/themes/Basetheme/templates/part-quick-links.php
@@ -31,10 +31,42 @@
             // no posts found
             
         }
+
         
         // Restore original Post Data
         unset($quick_links_side);
         wp_reset_postdata();
+        ?>
+        <?php
+
+        // check if the flexible content field has rows of data
+        if( have_rows('external_links') ):
+
+             // loop through the rows of data
+            while ( have_rows('external_links') ) : the_row();
+
+                if( get_row_layout() == 'external_url' ):?>
+
+                <li>
+                <a href="<?php echo get_sub_field('url') ?>" target="_blank"><?php the_sub_field('name'); ?></a>
+                 </li>
+
+        <?php   elseif( get_row_layout() == 'file_link' ): ?>
+
+                <li>
+                <a href="<?php echo get_sub_field('link') ?>" target="_blank"><?php the_sub_field('name'); ?></a>
+                </li>
+
+        <?php   endif;
+
+            endwhile;
+
+        else :
+
+            // no layouts found
+
+        endif;
+        
         ?>
         </ul>
 </div>
